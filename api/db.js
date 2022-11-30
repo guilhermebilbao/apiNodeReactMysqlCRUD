4998c27f078aa1dc9b74d4cbbe8f657261368819
Guilherme Bilbao Soares da Silva <guilhermebilbao@gmail.com>
2022-11-30 02:23:00 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "#lorena120607",
+    password: "*******",
     database: "crud2"
 });
